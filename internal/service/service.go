83d78eb2b918dd743c7177bc77ee93601c2ed9e8
JagadeesEgov <jagadeeswar@egovernments.org>
2025-07-31 09:37:48 +0530

    Update service.go

--- internal/service/service.go
+++ internal/service/service.go
@@ -22,37 +22,7 @@ func NewURLConverterService(repo repository.URLRepository) *URLConverterService 
 	return &URLConverterService{repo: repo}
 }
 
-// func (s *URLConverterService) ShortenHandler(c *gin.Context) {
-// 	var req models.ShortenRequest
-// 	if err := c.ShouldBindJSON(&req); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
-// 		return
-// 	}
-// 	if !utils.ValidateURL(req.URL) {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid URL"})
-// 		return
-// 	}
 
-// 	shortKey, err := s.repo.GetOrCreateShortKey(req)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	host := os.Getenv("HOST_NAME")
-// 	contextPath := os.Getenv("SERVER_CONTEXT_PATH")
-// 	// Ensure proper slashes
-// 	if !strings.HasSuffix(host, "/") {
-// 		host += "/"
-// 	}
-// 	if strings.HasPrefix(contextPath, "/") {
-// 		contextPath = contextPath[1:]
-// 	}
-// 	if contextPath != "" && !strings.HasSuffix(contextPath, "/") {
-// 		contextPath += "/"
-// 	}
-// 	shortURL := host + contextPath + shortKey
-// 	c.JSON(http.StatusOK, gin.H{"shortUrl": shortURL})
-// }
 func (s *URLConverterService) ShortenHandler(c *gin.Context) {
 	var req models.ShortenRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
